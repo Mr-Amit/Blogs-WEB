32a06104e18ccefaa87632ee20772261f499a565
Mr-Amit <amit.kumar221099@gmail.com>
2021-09-04 05:22:58 +0530

    CRUD Done

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 # this is mostly used for rendering 
 # and the 3rd argument should be a dictionary
 
+from django.contrib.messages.api import success
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import (
     ListView, 
     DetailView, 
-    CreateView)
-from django.contrib.auth.mixins import LoginRequiredMixin
+    CreateView,
+    UpdateView,
+    DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     context = {
@@ -31,7 +34,29 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
     
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
