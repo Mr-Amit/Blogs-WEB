bc2c7ad013e76b974f6a81d37d250573f12d1ade
Mr-Amit <amit.kumar221099@gmail.com>
2021-09-04 08:51:21 +0530

    Email Confirmation and Password Reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,3 +138,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER     = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
